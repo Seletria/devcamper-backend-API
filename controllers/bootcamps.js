480c0ef9689946c7238f6e8adfd210f3c92b1daa
Seletria <esra.kocaoglu.contact@gmail.com>
2026-04-21 22:58:36 +0300

    changes

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,11 +1,7 @@
-
 // @desc.    Get all bootcamps
 // @route    GET /api/v1/bootcamps
 // @access   Public
-
 exports.getBootcamps = (req, res, next) => {
-
-  throw new Error()
   res.status(200).json({ success: true, msg: 'Show all bootcamps' })
 
 }
