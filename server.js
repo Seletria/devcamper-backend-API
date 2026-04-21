dc3d9acc94fa8b68e201af0a7d0c4265e7d7ee2b
Seletria <esra.kocaoglu.contact@gmail.com>
2026-04-21 11:15:11 +0300

    using express router

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps')
+
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Mount routes 
+//bunu yazdigimiz icin, routes icinde ayni url'i yazmamiza gerek kalmadi.
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5001;
 
